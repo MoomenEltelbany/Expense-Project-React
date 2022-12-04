243502f1fc2ef2894cd1295ec3f4875549dcdf5d
Moomen Eltelbany <moomenmohamed4@gmail.com>
2022-12-04 16:43:18 -0500

    update the ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,26 +4,19 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
+  const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle("Updated");
-    };
-
-    return (
-        <>
-            <Card className="expense-item">
-                <ExpenseDate date={props.date} />
-                <div className="expense-item__description">
-                    <h2>{title}</h2>
-                    <div className="expense-item__price">
-                        {props.amount} CAD
-                    </div>
-                </div>
-                <button onClick={clickHandler}>Change Title</button>
-            </Card>
-        </>
-    );
+  return (
+    <>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">{props.amount} CAD</div>
+        </div>
+      </Card>
+    </>
+  );
 };
 
 export default ExpenseItem;
