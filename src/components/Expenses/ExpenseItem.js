5d6b4c4864c6749a0647965cfe18d763259e4f92
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2022-12-05 17:28:25 -0500

    update ExpenseItem
    Just removed the demo button of change title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,19 +4,19 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  return (
-    <>
-      <Card className="expense-item">
-        <ExpenseDate date={props.date} />
-        <div className="expense-item__description">
-          <h2>{title}</h2>
-          <div className="expense-item__price">{props.amount} CAD</div>
-        </div>
-      </Card>
-    </>
-  );
+    return (
+        <>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">
+                        {props.amount} CAD
+                    </div>
+                </div>
+            </Card>
+        </>
+    );
 };
 
 export default ExpenseItem;
