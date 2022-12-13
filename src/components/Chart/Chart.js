f01e8f62b18dc8a1ff9d3609782cdb2a129e498e
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2022-12-13 18:34:10 -0500

    Started the Chart bar section

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Chart.css";
+
+const chart = () => {};
+
+export default chart;
---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./ChartBar.css";
+
+const chartBar = () => {};
+
+export default chartBar;
